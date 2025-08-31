ac6af0c3dc50b0cbb85f3decf58814911580e311
Sel00k <Sel00k@yandex.ru>
2025-08-31 23:25:37 +0300

    Attestations.txt

---
+++ src/Homeworks/Homework03/Task1.java
@@ -0,0 +1,13 @@
+package Homeworks.Homework03;
+
+import java.util.Scanner;
+
+public class Task1 {
+    //
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner( System.in );
+        System.out.printf( "Ваше имя : " );
+        String name = scanner.nextLine();
+        System.out.printf("Привет, %s!\n" , name );
+    }
+}
