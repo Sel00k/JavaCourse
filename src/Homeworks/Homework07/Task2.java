8735d64ebd65ae07e0aeca394f05c041f0af7ab6
Sel00k <Sel00k@yandex.ru>
2025-08-31 23:25:37 +0300

    Домашнее задание №7 , задание №2

--- src/Homeworks/Homework07/Task2.java
+++ src/Homeworks/Homework07/Task2.java
@@ -1,12 +1,44 @@
 package Homeworks.Homework07;
 
 /* === Домашнее задание №7 , задание №2 ===
+   С консоли на вход подается две строки s и t.
+   Необходимо вывести true, если одна строка является валидной анаграммой другой строки,
+   и false – если это не так
 */
+
+/* Для проверки:
+   ● Бейсбол – бобслей
+   ● Героин – регион
+   ● Клоака – околка
+*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Task2 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello, World!" );
+        Scanner scanner = new Scanner( System.in );
+
+        System.out.printf( "Введите первую строку > " );
+        String s = scanner.nextLine();
+        System.out.printf( "Введите вторую строку > " );
+        String t = scanner.nextLine();
+
+        System.out.println( stringAnagramm( s , t ) );
     }
 
+    public static boolean stringAnagramm( String s ,  String t )
+    {
+        if ( s.length() != t.length() ) {
+            return false;
+        }
+
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        Arrays.sort( sArray );
+        Arrays.sort( tArray );
+        return Arrays.equals( sArray , tArray );
+    }
 }
